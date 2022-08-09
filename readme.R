472684f10dfef4927da1426e6d3717e33059477c
mmrgiken <108039560+mmrgiken@users.noreply.github.com>
2022-08-09 12:05:44 +0900

    Update readme.R

--- readme.R
+++ readme.R
@@ -22,5 +22,6 @@
       ,tags$a("開発担当者にメールを送る：池田拓郎・林潔・坂口雅哉",target="_blank",href="mailto:t_ikeda@macromill.com;j_lin@macromill.com;m_sakaguchi@macromill.com")
       ,p()
       ,tags$a("Teamsに投稿：MMR不具合・修正・要望チャンネル", target="_blank",href="https://teams.microsoft.com/l/channel/19%3acc424a5c86f2444f996881c2a3658385%40thread.skype/%25E8%25B3%25AA%25E5%2595%258F%25E3%2583%25BB%25E4%25B8%258D%25E5%2585%25B7%25E5%2590%2588%25E3%2583%25BB%25E8%25A6%2581%25E6%259C%259B?groupId=05701d53-109a-4aab-9e54-0e56a208701a&tenantId=644e05ec-c931-445c-8b08-599939d29a31")
+      ,tags$a("ユーザーの追加申請はこちら", target="_blank",href="https://teams.microsoft.com/l/channel/19%3a30e0ec3c6dd843fa985f6df99e44e359%40thread.skype/%25E3%2583%25A6%25E3%2583%25BC%25E3%2582%25B6%25E3%2583%25BC%25E7%2599%25BB%25E9%258C%25B2%25E4%25BE%259D%25E9%25A0%25BC%25E3%2581%25AF%25E3%2581%2593%25E3%2581%25A1%25E3%2582%2589%25EF%25BC%2581?groupId=05701d53-109a-4aab-9e54-0e56a208701a&tenantId=644e05ec-c931-445c-8b08-599939d29a31")
     )
 )
